89b1b4be691e15e6956eaed0234d77aa26485e93
sushbsush <sushmithasuhu2@gmail.com>
2022-11-17 02:01:04 +0000

    sush edit

--- script10.sh
+++ script10.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "enter the number"
 read a
-if [ $a -eq 5 ]
+if [ $a -eq 5 ];
 then 
-     echo "the given number $a is equal to 5"
+     echo "The given number $a is equal to 5"
 fi
 
